6c0a66309d55997710aace8a2f0cfbf6224c9751
itc-s24016 <s24016@std.it-college.ac.jp>
2024-06-10 06:23:24 +0900

    優良可不可

---
+++ if2.py
@@ -0,0 +1,9 @@
+#s24016
+#条件式の練習
+score=60
+if score>=80:
+    print("やったね‼")
+    print("次もこの調子だ")
+elif score<=
+else:
+    print("残念でした。")
---
+++ if_kadai.py
@@ -0,0 +1,11 @@
+#s24016
+#score判定プログラム
+score=90
+if score>=80:
+    print("優")
+elif score>=70:
+    print("良")
+elif score>=60:
+    print("可")
+else:
+    print("不可")
