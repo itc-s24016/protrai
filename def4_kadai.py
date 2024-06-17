a1423aca433e8bc2e43d16f2258284ef51101c4f
itc-s24016 <s24016@std.it-college.ac.jp>
2024-06-17 15:46:38 +0900

    def4アレンジ

--- def4_kadai.py
+++ def4_kadai.py
@@ -1,14 +1,14 @@
 #s24016
 #def4アレンジ　メインの処理をmain()関数に行わせるプログラム
 import random
-def omikuji():
+
+def omikuji():                                  #ランダムにkujiの中の一つを返す関数
     kuji=["大吉","中吉","小吉","凶"]
     return random.choice(kuji)
 
-kekka=omikuji()
-
-
-def main():
+def main():                                      #エントリーポイントの定義
+    kekka=omikuji()
     print("結果は",kekka,"です")
 
-main()
+if __name__=="__main__":                  #if文の通りならmain関数を実行=omikuji関数が実行される
+    main()
