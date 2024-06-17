9c8d2a04cc17f2b3dbcdb8296f4df43f5543162b
itc-s24016 <s24016@std.it-college.ac.jp>
2024-06-17 14:47:34 +0900

    add def.py

---
+++ def3.py
@@ -0,0 +1,7 @@
+#s24016
+#def1.pyに関数を追加したプログラム
+
+def sayhello2(name):
+    print("こんばんは"+name+"さん。")
+
+sayhello2("ふたば")
---
+++ def4.py
@@ -0,0 +1,9 @@
+#s24016
+#おみくじプログラム(with関数)
+import random
+def omikuji():
+    kuji=["大吉","中吉","小吉","凶"]
+    return random.choice(kuji)
+
+kekka=omikuji()
+print("結果は",kekka,"です")
