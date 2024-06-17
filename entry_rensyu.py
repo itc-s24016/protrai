545bc38a31022d3bf37e53c93fd8f03b0c239de2
itc-s24016 <s24016@std.it-college.ac.jp>
2024-06-17 15:00:37 +0900

    a

---
+++ entry_rensyu.py
@@ -0,0 +1,10 @@
+#s24016
+#main()関数をエントリーポイントとして使用する練習
+
+def main():
+    print("Hello World!")
+
+print(__name__=="__main__")
+#__name__にはもともと__main__が入っている
+
+main()
--- if_kadai.py
+++ if_kadai.py
@@ -1,6 +1,6 @@
 #s24016
 #score判定プログラム
-score=90
+score=75
 if score>=80:
     print("優")
 elif score>=70:
@@ -9,4 +9,3 @@ elif score>=60:
     print("可")
 else:
     print("不可")
-
